a5c4b3889446eb5b0f0e1b475b42257a1d34d2d5
Altaf1021 <156635336+Altaf1021@users.noreply.github.com>
2026-02-14 16:38:08 +0530

    Time: 16 ms (10.65%) | Memory: 45.9 MB (9.44%) - LeetSync

--- 815-champagne-tower/champagne-tower.cpp
+++ 815-champagne-tower/champagne-tower.cpp
@@ -1,20 +1,32 @@
 class Solution {
 public:
     double solve(double p,double r,double c,vector<vector<double>>&dp){
-    if(r==0 && c==0)return p;
-    if(r<0 || c<0 || c>r)return 0.0;
-    if(dp[r][c]!=-1.0)return dp[r][c];
-    double up_left=solve(p,r-1,c-1,dp);
-    up_left=max(0.0, (up_left - 1.0) / 2.0);
+        if(r==0 && c==0)return p;
+        if(r<0 || c<0 || c>r)return 0.0;
+        if(dp[r][c]!=-1.0)return dp[r][c];
+        double up_left=solve(p,r-1,c-1,dp);
+        up_left=max(0.0, (up_left - 1.0) / 2.0);
 
-    double up_right=solve(p,r-1,c,dp);
-    up_right=max(0.0, (up_right - 1.0) / 2.0);
+        double up_right=solve(p,r-1,c,dp);
+        up_right=max(0.0, (up_right - 1.0) / 2.0);
 
-    return dp[r][c]=up_left+up_right;
-}
+        return dp[r][c]=up_left+up_right;
+    }
     double champagneTower(int p, int r, int c) {
-        vector<vector<double>>dp(r+1,vector<double>(c+1,-1.0));
-        double ans=solve(p,r,c,dp);
-        return min(1.0, ans);
+        vector<vector<double>>dp(101,vector<double>(101,0.0));
+        // double ans=solve(p,r,c,dp);
+        dp[0][0]=p;
+        for(int i=0;i<r;i++){
+            for(int j=0;j<=i;j++){
+                if(dp[i][j]>1.0){
+                    double overflow=((dp[i][j]-1)/2);
+                    dp[i+1][j]+=overflow;
+                    dp[i+1][j+1]+=overflow;
+                }
+            }
+        }
+
+
+        return min(1.0, dp[r][c]);
     }
 };
